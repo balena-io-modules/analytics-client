03696b485baf9de95251c7f53e024bad5c3bee2b
Roman Mazur <roman@balena.io>
2019-10-18 12:49:47 +0300

    Fix device ID cookie path
    Change-type: patch
    Signed-off-by: Roman Mazur <roman@balena.io>

--- typings/cookie-utils.d.ts
+++ typings/cookie-utils.d.ts
@@ -1,7 +1,14 @@
 declare module '@analytics/cookie-utils' {
 	export function hasCookieSupport(): boolean;
 
-	export function setCookie(name: string, value: string): void;
+	export function setCookie(
+		name: string,
+		value: string,
+		ttlMs: number,
+		path: string,
+	): void;
 
 	export function getCookie(name: string): string;
+
+	export function removeCookie(name: string): void;
 }
