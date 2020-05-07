1e12b4790178dfe70286495e483d446746dc8ff5
Roman Mazur <roman@balena.io>
2020-05-07 14:21:20 +0300

    Add generic track to webtracker
    Note that the interface of trackPageView method is changed.
    
    Change-type: minor
    Signed-off-by: Roman Mazur <roman@balena.io>

--- test/web.test.ts
+++ test/web.test.ts
@@ -1,12 +1,11 @@
 import { createClient } from '../src/client';
 import { createWebTracker } from '../src/web';
 
-test('trackPageView', () => {
+describe('WebTracker', () => {
 	const client = createClient({
 		projectName: 'balena-test',
 		componentName: 'test',
 	});
-	const tracker = createWebTracker(client);
 
 	let passedEventType: string = '';
 	let passedData: any = null;
@@ -16,15 +15,53 @@ test('trackPageView', () => {
 		return 0;
 	};
 
-	tracker.trackPageView();
-	expect(passedEventType).toStrictEqual('Page View');
-	expect(passedData).toHaveProperty('metrics');
-	expect(passedData).toHaveProperty('current_url');
-	expect(passedData).toHaveProperty('current_url_path');
+	const ensurePageViewProperties = () => {
+		expect(passedData).toHaveProperty('metrics');
+		expect(passedData).toHaveProperty('current_url');
+		expect(passedData).toHaveProperty('current_url_path');
+	};
 
-	tracker.trackPageView('prefix');
-	expect(passedEventType).toStrictEqual('[prefix] Page View');
+	describe('With prefix', () => {
+		const tracker = createWebTracker(client, 'prefix');
 
-	tracker.trackPageView('', 'test event');
-	expect(passedEventType).toStrictEqual('test event');
+		it('tracks generic event', () => {
+			tracker.track('event', { p1: 'v1', p2: 42 });
+			expect(passedEventType).toStrictEqual('[prefix] event');
+			expect(passedData).toBeTruthy();
+			expect(passedData.p1).toStrictEqual('v1');
+			expect(passedData.p2).toStrictEqual(42);
+		});
+
+		it('tracks page view', () => {
+			tracker.trackPageView();
+			expect(passedEventType).toStrictEqual('[prefix] Page View');
+			ensurePageViewProperties();
+
+			tracker.trackPageView('test event');
+			expect(passedEventType).toStrictEqual('[prefix] test event');
+			ensurePageViewProperties();
+		});
+	});
+
+	describe('No prefix', () => {
+		const tracker = createWebTracker(client);
+
+		it('tracks generic event', () => {
+			tracker.track('event', { p1: 'v1', p2: 42 });
+			expect(passedEventType).toStrictEqual('event');
+			expect(passedData).toBeTruthy();
+			expect(passedData.p1).toStrictEqual('v1');
+			expect(passedData.p2).toStrictEqual(42);
+		});
+
+		it('tracks page view', () => {
+			tracker.trackPageView();
+			expect(passedEventType).toStrictEqual('Page View');
+			ensurePageViewProperties();
+
+			tracker.trackPageView('test event');
+			expect(passedEventType).toStrictEqual('test event');
+			ensurePageViewProperties();
+		});
+	});
 });
