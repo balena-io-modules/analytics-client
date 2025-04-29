e491ef9750a0ce9ccb11365ee5e95d6fb6925e20
myarmolinsky <yarmolinskymatthew@gmail.com>
2025-04-29 16:33:13 -0400

    Remove `@amplitude/engagement-browser`
    Change-type: patch

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,4 @@
 import { Identify, Types, createInstance } from '@amplitude/analytics-browser';
-import { plugin as engagementPlugin } from '@amplitude/engagement-browser';
 import { userAgentEnrichmentPlugin } from '@amplitude/plugin-user-agent-enrichment-browser';
 import { version } from '../package.json';
 import { getAmplitudeEndpoint } from './common';
@@ -91,7 +90,6 @@ class DefaultClient implements Client {
 
 	constructor(config: Config) {
 		this.amplitudeInstance = createInstance();
-		this.amplitudeInstance.add(engagementPlugin());
 		this.amplitudeInstance.add(userAgentEnrichmentPlugin());
 
 		const amplConfig: Types.BrowserOptions = Object.assign(
