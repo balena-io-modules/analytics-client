35e2bf291d1d274a97e0eff90782b28695dad172
Matthew Yarmolinsky <yarmolinskymatthew@gmail.com>
2025-09-08 09:32:17 -0400

    Add `createExperimentClient` function for initializing Experiment Client
    Change-type: minor

--- index.ts
+++ index.ts
@@ -1,5 +1,11 @@
 export { AnalyticsUrlParams } from './src/url-params';
-export { Client, Config, createClient, createNoopClient } from './src/client';
+export {
+	Client,
+	Config,
+	createClient,
+	createNoopClient,
+	createExperimentClient,
+} from './src/client';
 export { createMarketingClient } from './src/marketingClient';
 export { Experiment, LocalExperiment } from './src/experiment';
 export { WebTracker, createWebTracker } from './src/web';
